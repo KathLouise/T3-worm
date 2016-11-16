ceb3bff2d90623ae23f6e52468657bffc80829ca
Katheryne <kathe.nightwish@gmail.com>
2016-11-16 14:33:46 +0000

    bruteforce iniciado, já está pegando login e senhas, falta testar até achar a senha que bate

--- bruteforce.h
+++ bruteforce.h
@@ -3,6 +3,7 @@
 
 void iterate(char *str, const char *base[], int idx, int len, FILE *output);
 void keyGenerator(unsigned int len);
+int match(char *user, char *pass)
 int bruteforce(unsigned int lenKey);
 
 #endif
